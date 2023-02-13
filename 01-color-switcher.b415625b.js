c9c70ed72356f1cc57c3a802e5e4e6596111d939
dyonns <117724779+dyonns@users.noreply.github.com>
2023-02-13 21:40:30 +0000

    Deploying to gh-pages from @ dyonns/goit-js-hw-09@5358bcf92d8e03c27f6f701b534d913cdc80f4fc 🚀

---
+++ 01-color-switcher.b415625b.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=null;e.disabled=!0,t.addEventListener("click",(function(){function d(){document.body.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16))}t.disabled=!0,e.disabled=!1,d(),n=setInterval(d,1e3)})),e.addEventListener("click",(function(){clearInterval(n),t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.b415625b.js.map
